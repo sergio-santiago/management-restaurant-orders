03ffcd4bbf59201048103af83e2c6c34ba551891
Sergio Santiago <sersanhen@gmail.com>
2018-05-02 02:40:26 +0200

    ADD: vista y modelo de seleccion mesas

---
+++ src/model/sistema_pedidos/MesasModel.java
@@ -0,0 +1,60 @@
+package model.sistema_pedidos;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import model.DatabaseConnectionService;
+
+public class MesasModel {
+	public String[] getNombreMesasDisponibles() {
+		ArrayList<String> nombreMesasDisponibles = new ArrayList<String>();
+		//Sacamos de BBDD
+		DatabaseConnectionService.openConnection();
+		Statement statement = null;
+		ResultSet resultSet = null;
+		try {
+			//Hacemos consulta
+			statement = DatabaseConnectionService.getConnection().createStatement();
+			String query = "select * from " + DatabaseConnectionService.getDatabase() + ".MESA minus select m.* from " + DatabaseConnectionService.getDatabase() + ".MESA m, " + DatabaseConnectionService.getDatabase() + ".PEDIDO p where m.\"ID\" = p.ID_MESA and p.EN_CURSO = 'true'";
+			resultSet = statement.executeQuery(query);
+			while(resultSet.next()) {
+				nombreMesasDisponibles.add(resultSet.getString("nombre"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+		    try { if (resultSet != null) resultSet.close(); } catch (Exception e) {};
+		    try { if (statement != null) statement.close(); } catch (Exception e) {};
+		    try { if (DatabaseConnectionService.getConnection() != null) DatabaseConnectionService.closeConnection(); } catch (Exception e) {};
+		}
+		String[] nombreMesasArray = nombreMesasDisponibles.toArray(new String[nombreMesasDisponibles.size()]);
+		return nombreMesasArray;
+	}
+	
+	public String[] getNombreMesasOcupadas() {
+		ArrayList<String> nombreMesasOcupadas = new ArrayList<String>();
+		//Sacamos de BBDD
+		DatabaseConnectionService.openConnection();
+		Statement statement = null;
+		ResultSet resultSet = null;
+		try {
+			//Hacemos consulta
+			statement = DatabaseConnectionService.getConnection().createStatement();
+			String query = "select m.* from " + DatabaseConnectionService.getDatabase() + ".MESA m, " + DatabaseConnectionService.getDatabase() + ".PEDIDO p where m.\"ID\" = p.ID_MESA and p.EN_CURSO = 'true'";
+			resultSet = statement.executeQuery(query);
+			while(resultSet.next()) {
+				nombreMesasOcupadas.add(resultSet.getString("nombre"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+		    try { if (resultSet != null) resultSet.close(); } catch (Exception e) {};
+		    try { if (statement != null) statement.close(); } catch (Exception e) {};
+		    try { if (DatabaseConnectionService.getConnection() != null) DatabaseConnectionService.closeConnection(); } catch (Exception e) {};
+		}
+		String[] nombreMesasArray = nombreMesasOcupadas.toArray(new String[nombreMesasOcupadas.size()]);
+		return nombreMesasArray;
+	}
+	
+}
---
+++ src/view/sistema_pedidos/MesasView.java
@@ -0,0 +1,86 @@
+package view.sistema_pedidos;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Font;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+
+public class MesasView extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JButton btnAtras;
+	private JLabel lblNoMesas;
+	private JButton[] mesasButtons;
+
+	/**
+	 * Create the frame.
+	 */
+	public MesasView(String[] mesasButtonNames) {
+		//jFrame
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1366, 768);
+		
+		//contentPane
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		//btnAtras
+		btnAtras = new JButton("<< Atras");
+		btnAtras.setBounds(10, 11, 89, 23);
+		contentPane.add(btnAtras);
+		
+		//panelMesas
+		JPanel panelMesas = new JPanel();
+		panelMesas.setBounds(10, 45, 1330, 673);
+		contentPane.add(panelMesas);
+		panelMesas.setLayout(new GridLayout(5, 5, 0, 0));
+		
+		if(mesasButtonNames.length == 0) {
+			//lblNoMesas
+			lblNoMesas = new JLabel("No hay ninguna mesa en estos momentos");
+			lblNoMesas.setHorizontalAlignment(SwingConstants.CENTER);
+			lblNoMesas.setFont(new Font("Tahoma", Font.PLAIN, 50));
+			lblNoMesas.setForeground(Color.RED);
+			panelMesas.add(lblNoMesas);
+		} else {
+			//bucle creacion de botones de mesas dinamicamente
+			this.mesasButtons = new JButton[mesasButtonNames.length];
+			for(int i = 0; i < this.mesasButtons.length; i++) {
+				JButton btnMesa = new JButton(mesasButtonNames[i]);
+				btnMesa.setFont(new Font("Tahoma", Font.PLAIN, 20));
+				mesasButtons[i] = btnMesa;
+				panelMesas.add(btnMesa);
+			}
+		}
+		
+		setVisible(true);
+	}
+	
+	/**
+	 * Components Getters
+	 */
+	public JPanel getContentPane() {
+		return contentPane;
+	}
+
+	public JButton getBtnAtras() {
+		return btnAtras;
+	}
+
+	public JLabel getLblNoMesas() {
+		return lblNoMesas;
+	}
+
+	public JButton[] getMesasButtons() {
+		return mesasButtons;
+	}
+
+}
